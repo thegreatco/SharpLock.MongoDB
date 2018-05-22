315bde72a74cf60c2d0303b91e0389fd0d8af864
TheGreatCO <thegreatco@thegreatco.com>
2018-05-21 21:33:33 -0400

    initial commit

---
+++ src/SharpLock.MongoDB/SharpLockMongoDataStore.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace SharpLock.MongoDB
+{
+    public class SharpLockMongoDataStore<TLockableObject> : ISharpLockDataStore<TLockableObject, ObjectId>
+        where TLockableObject : class, ISharpLockable<ObjectId>
+    {
+        private readonly SharpLockMongoDataStore<TLockableObject, TLockableObject> _baseDataStore;
+
+        public SharpLockMongoDataStore(IMongoCollection<TLockableObject> col, ISharpLockLogger sharpLockLogger, TimeSpan lockTime)
+        {
+            _baseDataStore = new SharpLockMongoDataStore<TLockableObject, TLockableObject>(col, sharpLockLogger, lockTime);
+        }
+
+        public ISharpLockLogger GetLogger() => _baseDataStore.GetLogger();
+        public TimeSpan GetLockTime() => _baseDataStore.GetLockTime();
+
+        public Task<TLockableObject> AcquireLockAsync(ObjectId baseObjId, TLockableObject obj, int staleLockMultiplier,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _baseDataStore.AcquireLockAsync(baseObjId, obj, x => x, staleLockMultiplier, cancellationToken);
+        }
+
+        public Task<bool> RefreshLockAsync(ObjectId baseObjId, Guid lockedObjectLockId,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _baseDataStore.RefreshLockAsync(baseObjId, baseObjId, lockedObjectLockId, x => x, cancellationToken);
+        }
+
+        public Task<bool> ReleaseLockAsync(ObjectId baseObjId, Guid lockedObjectLockId,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _baseDataStore.ReleaseLockAsync(baseObjId, baseObjId, lockedObjectLockId, x => x, cancellationToken);
+        }
+
+        public Task<TLockableObject> GetLockedObjectAsync(ObjectId baseObjId, Guid lockedObjectLockId,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _baseDataStore.GetLockedObjectAsync(baseObjId, baseObjId, lockedObjectLockId, x => x,
+                cancellationToken);
+        }
+    }
+}
